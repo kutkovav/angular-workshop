8de69c2f01955e705ea6161a10fc628fc417c576
VER <ver@cca.cz>
2021-03-02 10:49:33 +0100

    DDD Labs - Bonus part

--- libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
+++ libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
 import { CheckinComponent } from './checkin.component';
-import {RouterModule} from "@angular/router";
+import {RouterModule} from '@angular/router';
+import {LuggageUiCardModule} from '@flight-workspace/luggage/ui-card';
+import {LoggerModule} from '@flight-workspace/logger-lib';
 
 @NgModule({
   imports: [
     CommonModule,
     LuggageDomainModule,
+    LuggageUiCardModule,
+    LoggerModule,
     RouterModule.forChild([
       {
         path: '',
